00b3a88b8651109a7c09e5b09140609eb71d615f
uttam282005 <uttam282005@gmail.com>
2025-10-04 23:19:29 +0530

    feat(worker): refactor worker

--- mapReduce/mr/worker.go
+++ mapReduce/mr/worker.go
@@ -4,9 +4,11 @@ import (
 	"encoding/json"
 	"fmt"
 	"hash/fnv"
+	"io"
 	"log"
 	"net/rpc"
 	"os"
+	"time"
 )
 
 // Map functions return a slice of KeyValue.
@@ -19,8 +21,14 @@ type GetJobArgs struct{}
 
 type GetJobReply struct {
 	FileName string
-	Content  string
+	JobId    int
+	NReduce  int
+	JobType  string 
 }
+
+type JobDoneArgs struct{}
+
+type JobDoneReply struct{}
 
 // use ihash(key) % NReduce to choose the reduce
 // task number for each KeyValue emitted by Map.
@@ -30,55 +38,92 @@ func ihash(key string) int {
 	return int(h.Sum32() & 0x7fffffff)
 }
 
-func check(err any) {
+func handleMapJob(
+	fileName string,
+	mapf func(string, string) []KeyValue,
+	mapTaskId int,
+	nReduce int,
+) bool {
+	inputFile, err := os.Open(fileName)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("cannot read %v", fileName)
 	}
-}
+	defer inputFile.Close()
 
-func handleMapJob(fileName string, content string, mapf func(string, string) []KeyValue, mapTaskNumber int) bool {
-	mapgoResult := mapf(fileName, content)
-	reduceTaskNumber := ihash(fileName)
-	f, err := os.Create(fmt.Sprintf("mr-%d-%d", mapTaskNumber, reduceTaskNumber))
+	content, err := io.ReadAll(inputFile)
 	if err != nil {
-		log.Fatal("cannot create map output file")
-		return false
+		log.Fatalf("cannot read %v", fileName)
 	}
-	defer f.Close()
-	enc := json.NewEncoder(f)
-	for _, kv := range mapgoResult {
-		err := enc.Encode(&kv)
+
+	mapgoResult := mapf(fileName, string(content))
+
+	enc := make([]*json.Encoder, nReduce)
+
+	for r := range nReduce {
+		intermediateFile := fmt.Sprintf("mr-%d-%d", mapTaskId, r)
+		f, err := os.Create(intermediateFile)
 		if err != nil {
-			log.Fatal("cannot encode kv pair")
-			return false
+			log.Fatalf("cannot create intermediate file %v", intermediateFile)
+		}
+		defer f.Close()
+		enc[r] = json.NewEncoder(f)
+	}
+
+	for _, kv := range mapgoResult {
+		reduceTaskId := ihash(kv.Key) % nReduce
+		err := enc[reduceTaskId].Encode(&kv)
+		if err != nil {
+			log.Fatal("cannot encode map result")
 		}
 	}
+
 	return true
 }
 
-func Worker(mapf func(string, string) []KeyValue,
+
+func Worker(
+	mapf func(string, string) []KeyValue,
 	reducef func(string, []string) string,
 ) {
-	mapTaskNumber := 0
-	args := GetJobArgs{}
-	reply := GetJobReply{}
-
 	for {
-		// ask for a map job
-		ok := call("Coordinator.GetJob", &args, &reply)
-		if ok {
-			mapTaskNumber++
-			go handleMapJob(reply.FileName, reply.Content, mapf, mapTaskNumber)
-		} else {
+		args := GetJobArgs{}
+		reply := GetJobReply{}
+
+		if !call("Coordinator.GetJob", &args, &reply) {
+			log.Println("Worker: coordinator unavailable, retrying...")
+			time.Sleep(2 * time.Second)
+			continue
+		}
+
+		// Check if there’s actually a job
+		if reply.FileName == "" {
+			log.Println("Worker: no more jobs, exiting")
 			break
 		}
+
+		// Copy reply to avoid data race
+		job := reply
+
+		go func(job GetJobReply) {
+			ok := handleMapJob(job.FileName, mapf, job.JobId, reply.NReduce)
+			if ok {
+				doneArgs := JobDoneArgs{}
+				doneReply := JobDoneReply{}
+				call("Coordinator.Done", &doneArgs, &doneReply)
+			} else {
+				log.Printf("Worker: job %d failed on file %v", job.JobId, job.FileName)
+			}
+		}(job)
+
+		time.Sleep(500 * time.Millisecond) // avoid hot-looping
 	}
 }
+
 
 // send an RPC request to the coordinator, wait for the response.
 // usually returns true.
 // returns false if something goes wrong.
-func call(rpcname string, args interface{}, reply interface{}) bool {
+func call(rpcname string, args any, reply any) bool {
 	// c, err := rpc.DialHTTP("tcp", "127.0.0.1"+":1234")
 	sockname := coordinatorSock()
 	c, err := rpc.DialHTTP("unix", sockname)
